c2c4ddedd58c81df1ea22980e2d22e92ba7b9d41
jpcoderesearch <jpcoderesearch@gmail.com>
2018-03-28 23:46:27 +0530

    blog changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315172527) do
+ActiveRecord::Schema.define(version: 20180328173657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,12 @@ ActiveRecord::Schema.define(version: 20180315172527) do
     t.integer  "user_id"
     t.text     "description"
     t.string   "image"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "boarding_forms", force: :cascade do |t|
@@ -47,12 +51,39 @@ ActiveRecord::Schema.define(version: 20180315172527) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "mile_services", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "packages", force: :cascade do |t|
     t.string   "name"
     t.integer  "days"
     t.integer  "nights"
     t.decimal  "amount"
     t.text     "description"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.string   "Day1"
+    t.string   "Day2"
+    t.string   "Day3"
+    t.string   "Day4"
+    t.string   "Day5"
+    t.string   "Day6"
+    t.string   "Day7"
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image"
+    t.text     "Details"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.string   "image_file_name"
